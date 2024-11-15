2ae089e9a5df8857e418a3b1579559bee925d439
cher1shRXD <taewoo080401@gmail.com>
2024-11-15 16:20:48 +0900

    feat: register endpoint

--- routes/playRouter.js
+++ routes/playRouter.js
@@ -2,9 +2,11 @@ const express = require('express');
 const playNext = require('../services/play/playNext');
 const playPrevious = require('../services/play/playPrevious');
 const auth = require('../middleware/auth');
+const updateShuffle = require('../services/play/updateShuffle');
 const playRouter = express.Router();
 
 playRouter.get('/next', auth, playNext);
 playRouter.get("/previous", auth, playPrevious);
+playRouter.patch('/update-shuffle', auth, updateShuffle);
 
 module.exports = playRouter;
