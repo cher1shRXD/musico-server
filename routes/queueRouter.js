572169ceec5ec75a877171b12e31292f5d5241ac
cher1shRXD <taewoo080401@gmail.com>
2024-11-17 11:39:22 +0900

    feat: play playlist

--- routes/queueRouter.js
+++ routes/queueRouter.js
@@ -3,10 +3,12 @@ const auth = require("../middleware/auth");
 const addSong = require("../services/queue/addSong");
 const deleteSong = require("../services/queue/deleteSong");
 const addLast = require("../services/queue/addLast");
+const copyPlaylist = require("../services/queue/copyPlaylist");
 const queueRouter = express.Router();
 
 queueRouter.post("/", auth, addSong);
 queueRouter.delete('/', auth, deleteSong);
 queueRouter.post('/add', auth, addLast);
+queueRouter.post('/copy', auth, copyPlaylist);
 
 module.exports = queueRouter;
---
+++ services/queue/copyPlaylist.js
@@ -0,0 +1,20 @@
+const Playlist = require("../../models/playlist");
+const User = require("../../models/user");
+
+const copyPlaylist = async (req, res) => {
+  try {
+    const { playlistId } = req.query;
+    const user = await User.findById(req.user.id);
+    const playlist = await Playlist.findOne({ id: playlistId });
+
+    user.queue = playlist.songs;
+    user.currentSong = 0;
+
+    user.save();
+    res.status(200).json(user);
+  } catch {
+    res.status(500).json({ message: "SERVER_ERROR" });
+  }
+};
+
+module.exports = copyPlaylist;
